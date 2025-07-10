dc2db3de31b9fccb5a936aec5041693edf55fcfd
Nikita-kanwar <kanwarnikita2025@gmail.com>
2025-07-10 16:51:58 +0530

    React app

---
+++ day6-react-intro/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <h1>My Site</h1>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
