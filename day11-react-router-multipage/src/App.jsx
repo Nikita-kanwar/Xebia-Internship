1e07d2b37bce242549e425444181e4eb1eaa54ca
Nikita-kanwar <kanwarnikita2025@gmail.com>
2025-07-21 15:29:10 +0530

    Route diffferent Pages

---
+++ day11-react-router-multipage/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home'
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ day11-react-router-multipage/src/Pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This project was built by an intern learning React Router.</p>
+    </div>
+  );
+};
+
+export default About;
