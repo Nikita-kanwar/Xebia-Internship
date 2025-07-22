0f909f3e905e9da35c3e863d88fe40fe21bf6662
Nikita-kanwar <kanwarnikita2025@gmail.com>
2025-07-22 15:43:39 +0530

    Update

---
+++ day12-react-forms-validation/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Signup from './Pages/Signup'
+import InputField from './Components/InputField'
+
+const App = () => {
+  return (
+    <div>
+      <InputField/>
+      <Signup/>
+    </div>
+  )
+}
+
+export default App
