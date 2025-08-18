a5462af4ef56ec33df72bcee60e6a740968afd43
Nikita-kanwar <kanwarnikita2025@gmail.com>
2025-08-18 12:01:28 +0530

    Update

--- Fontend Daily Tasks/day4-todo-app/script.js
+++ Fontend Daily Tasks/day4-todo-app/script.js
@@ -1,54 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const inputBox = document.getElementById("inputbox");
-    const addBtn = document.getElementById("addbtn");
-    const todoList = document.querySelector(".todoList");
+  const inputBox = document.getElementById("inputbox");
+  const addBtn = document.getElementById("addbtn");
+  const todoList = document.querySelector(".todoList");
 
-    addBtn.addEventListener("click", () => {
-        const taskText = inputBox.value.trim();
+  addBtn.addEventListener("click", () => {
+    const taskText = inputBox.value.trim();
 
-        if (taskText !== "") {
-            const newTask = document.createElement("li");
+    if (taskText !== "") {
+      const newTask = document.createElement("li");
 
-            newTask.innerHTML = ` <p>${taskText}</p><button class="btn deletebtn">Cancel</button>`;
-           
-            todoList.appendChild(newTask);
- 
-            inputBox.value = "";
-            
-            checkEmptyList();
-        }
-    });
+      newTask.innerHTML = ` <p>${taskText}</p><button class="btn deletebtn">Cancel</button>`;
 
-    
-    inputBox.addEventListener("keypress", (event) => {
-        if (event.key === "Enter") {
-            addBtn.click();
-        }
-    });
+      todoList.appendChild(newTask);
 
-   
-    
-    todoList.addEventListener("click", (event) => {
-        if (event.target.classList.contains("deletebtn")) {
-            const confirmation = confirm("Are you sure you want to delete this task?");
-            if (confirmation) {
-                event.target.parentElement.remove();
-                checkEmptyList();
-            }
-        }
-    });
+      inputBox.value = "";
 
-   
-    function checkEmptyList() {
-        const noTasksMessage = document.getElementById("noTasksMessage");
-
-        if (todoList.children.length === 0) {
-            
-            noTasksMessage.style.display = "block";
-        } else {  noTasksMessage.style.display = "none";
-        }
+      checkEmptyList();
     }
+  });
 
-   
-    checkEmptyList();
+  inputBox.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      addBtn.click();
+    }
+  });
+
+  todoList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("deletebtn")) {
+      const confirmation = confirm(
+        "Are you sure you want to delete this task?"
+      );
+      if (confirmation) {
+        event.target.parentElement.remove();
+        checkEmptyList();
+      }
+    }
+  });
+
+  function checkEmptyList() {
+    const noTasksMessage = document.getElementById("noTasksMessage");
+
+    if (todoList.children.length === 0) {
+      noTasksMessage.style.display = "block";
+    } else {
+      noTasksMessage.style.display = "none";
+    }
+  }
+
+  checkEmptyList();
 });
