8555d08dbec0c9cf0c324fd5c4f2379a851a54c4
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-08-23 17:51:48 +0530

    Backend Tasks

---
+++ Backend Mini Tasks/Task1/Script.js
@@ -0,0 +1,14 @@
+// script that logs system info (OS type, free memory, file path of current directory).
+const os = require('os');
+const path = require('path');
+
+console.log("OS Type:", os.type());
+console.log("Free Memory:", os.freemem());
+console.log("Total Memory:", os.totalmem());
+
+console.log("Current Directory:", __dirname);
+console.log("Current File:", __filename);
+console.log("File Path Example:", path.join(__dirname, 'systemInfo.js'));
+
+console.log("Process ID:", process.pid);
+console.log("Node Version:", process.version);
---
+++ Backend Mini Tasks/Task2/app.js
@@ -0,0 +1,10 @@
+
+const math = require('./math');
+
+const {  add,subtract, multiply,divide} = math;
+
+console.log("Addition:", add(5, 3));
+console.log("Subtraction:", subtract(10, 4));
+console.log("Multiplication:", multiply(6, 7));
+console.log("Division:", divide(20, 5));
+console.log("Division by zero:", divide(10, 0));
